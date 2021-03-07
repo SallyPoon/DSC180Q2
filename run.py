94fb9abebbf6de732ac99875e4dc80eb7fff6f48
phdeshmane <43420182+phdeshmane@users.noreply.github.com>
2021-03-07 01:08:09 -0800

    Update run.py

--- run.py
+++ run.py
@@ -2,23 +2,21 @@
 import sys
 import json
 #sys.path.insert(0, 'src')
-from src.test1 import test, tester, plotter
+from src.test import test
 from src.conversion import convert
 from src.viz_analysis import plot_all
 
 def main(targets):
     data_cfg = json.load(open('config/conversion.json'))
     viz_cfg = json.load(open('config/viz-params.json'))
-    # test_cfg = json.load(open('config/test-params.json'))
-    # test_vis_cfg = json.load(open('config/test-vis-params.json'))
     test_cfg = json.load(open('config/test.json'))
     
     if 'test' in targets:
         test(**test_cfg)
-        print('Completed. Plots are saved.')
+        print('Successfully Completed. Plots are saved.')
     if 'conversion' in targets:
         convert(**data_cfg)
-        print('Raw Data Bags converted to csv')
+        print('Raw Data Bags Extracted and Converted to csv')
     if 'viz_analysis' in targets:
         plot_all(**viz_cfg)
         print('Data plotted')
